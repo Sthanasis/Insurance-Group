aa8507658306adeeb1e27a252e7e6a849a522b67
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2021-02-17 02:19:02 +0200

    map :feature

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue';
 import App from './App.vue';
 import i18n from './translations/locale';
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 
 Vue.config.productionTip = false;
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+Vue.component('l-popup', LPopup);
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 new Vue({
   i18n,
